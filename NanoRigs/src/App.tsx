e669ecc33596d720a4a69545c8241bef23f53de1
Your Name <raultrusca@gmail.com>
2025-09-25 17:50:46 +0300

    header

---
+++ NanoRigs/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import Header from "./components/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="p-10"></main>
+    </>
+  );
+}
+
+export default App;
